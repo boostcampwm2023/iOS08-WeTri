fc5204a92455bcba557c0e84b54a02f1c3f00b32
SeungHyun Hong <whi7ehyun@gmail.com>
2023-11-10 23:39:42 +0900

    build: Tuist를 통한 WeTri App Project 생성

---
+++ iOS/Projects/App/Sources/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  WeTri
+//
+//  Created by 홍승현 on 11/10/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+  var window: UIWindow?
+  
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+    guard let windowScene = scene as? UIWindowScene else { return }
+    window = UIWindow(windowScene: windowScene)
+    window?.rootViewController = ViewController()
+    window?.makeKeyAndVisible()
+  }
+}
---
+++ iOS/Workspace.swift
@@ -0,0 +1,10 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+
+let workspace = Workspace(
+  name: "WeTri",
+  projects: [
+    "Projects/App",
+  ]
+)
