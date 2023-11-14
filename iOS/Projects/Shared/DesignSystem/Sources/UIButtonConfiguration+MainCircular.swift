3a810240fcf8ecf5dc1a2cfe7402b8b6e5309921
SeungHyun Hong <whi7ehyun@gmail.com>
2023-11-14 11:33:12 +0900

    feat: Design System 버튼 추가
    - 일반적으로 사용하는 메인 색깔의 둥근 모서리 버튼
    - 메인 색깔의 원형스타일 버튼
    - button state에 따라 스타일이 바뀌는 handler

---
+++ iOS/Projects/Shared/DesignSystem/Sources/UIButtonConfiguration+MainCircular.swift
@@ -0,0 +1,83 @@
+//
+//  UIButtonConfiguration+MainCircular.swift
+//  DesignSystem
+//
+//  Created by 홍승현 on 11/14/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Main Circular Button Configuration
+
+public extension UIButton.Configuration {
+  // MARK: UIButton.Configuration
+
+  static func mainCircularEnabled(title: String) -> UIButton.Configuration {
+    var plainConfiguration = Self.plain()
+
+    var backgroundConfiguration = plainConfiguration.background
+    backgroundConfiguration.backgroundColor = .main03
+    plainConfiguration.background = backgroundConfiguration
+
+    plainConfiguration.titleAlignment = .center
+    plainConfiguration.cornerStyle = .capsule
+    plainConfiguration.baseForegroundColor = .secondaryBackGround
+    plainConfiguration.title = title
+
+    return plainConfiguration
+  }
+
+  static func mainCircularDisabled(title: String) -> UIButton.Configuration {
+    var plainConfiguration = Self.plain()
+
+    var backgroundConfiguration = plainConfiguration.background
+    backgroundConfiguration.backgroundColor = .gray03
+    plainConfiguration.background = backgroundConfiguration
+
+    plainConfiguration.titleAlignment = .center
+    plainConfiguration.cornerStyle = .capsule
+    plainConfiguration.baseForegroundColor = .secondaryBackGround
+    plainConfiguration.title = title
+
+    return plainConfiguration
+  }
+
+  static func mainCircularSelected(title: String) -> UIButton.Configuration {
+    var plainConfiguration = Self.plain()
+
+    var backgroundConfiguration = plainConfiguration.background
+    backgroundConfiguration.backgroundColor = .main02
+    plainConfiguration.background = backgroundConfiguration
+
+    plainConfiguration.titleAlignment = .center
+    plainConfiguration.cornerStyle = .capsule
+    plainConfiguration.baseForegroundColor = .secondaryBackGround
+    plainConfiguration.title = title
+
+    return plainConfiguration
+  }
+
+  // MARK: - ConfigurationUpdateHandler
+
+  static func mainCircular(label text: String) -> UIButton.ConfigurationUpdateHandler {
+    return { button in
+      switch button.state {
+      case .normal:
+        button.configuration = .mainCircularEnabled(title: text)
+      case .selected:
+        button.configuration = .mainCircularSelected(title: text)
+      case .disabled:
+        button.configuration = .mainCircularDisabled(title: text)
+      default: break
+      }
+      button.configuration?.title = text
+      button.configuration?.font = UIFont(
+        descriptor: UIFont
+          .preferredFont(forTextStyle: .largeTitle)
+          .fontDescriptor.withSymbolicTraits(.traitBold)!,
+        size: 0
+      )
+    }
+  }
+}
