de29f38408fe25e33c3fa1ef50478c43264359b7
wonholim <kds0034@gmail.com>
2023-11-16 14:12:08 +0900

    chore: lint 적용

--- BackEnd/src/config/swagger.config.ts
+++ BackEnd/src/config/swagger.config.ts
@@ -2,13 +2,13 @@ import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const SwaggerSetting = (app: INestApplication) => {
-    const config = new DocumentBuilder()
-        .setTitle('Wetri')
-        .setDescription('API')
-        .setVersion('1.0')
-        .addTag('wonholim')
-        .build();
+  const config = new DocumentBuilder()
+    .setTitle('Wetri')
+    .setDescription('API')
+    .setVersion('1.0')
+    .addTag('wonholim')
+    .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 };
