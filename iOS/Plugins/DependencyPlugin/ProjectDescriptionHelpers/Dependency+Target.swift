8c87f5ab465f5a807b39ac97314c413c61b6eb33
SeungHyun Hong <whi7ehyun@gmail.com>
2023-11-19 22:54:00 +0900

    chore: Rename TNCocoaCombine to `CombineCocoa`

--- iOS/Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift
+++ iOS/Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift
@@ -19,7 +19,7 @@ public extension TargetDependency {
   static let designSystem: TargetDependency = .project(target: "DesignSystem", path: .relativeToRoot("Projects/Shared/DesignSystem"))
   static let trinet: TargetDependency = .project(target: "Trinet", path: .relativeToRoot("Projects/Core/Network"))
   static let coordinator: TargetDependency = .project(target: "Coordinator", path: .relativeToRoot("Projects/Core/Coordinator"))
-  static let combineCocoa: TargetDependency = .project(target: "TNCocoaCombine", path: .relativeToRoot("Projects/Shared/TNCocoaCombine"))
+  static let combineCocoa: TargetDependency = .project(target: "CombineCocoa", path: .relativeToRoot("Projects/Shared/CombineCocoa"))
 
   static func feature(_ feature: Feature) -> TargetDependency {
     return .project(
---
+++ iOS/Projects/Shared/CombineCocoa/Project.swift
@@ -0,0 +1,9 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.makeModule(
+  name: "CombineCocoa",
+  product: .framework,
+  resources: nil,
+  isTestable: false
+)
