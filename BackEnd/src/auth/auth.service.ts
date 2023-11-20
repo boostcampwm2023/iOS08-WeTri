92a920b44ea66738c63548220b5e88a79dd9853e
신정용 <shinjy0211@gmail.com>
2023-11-20 11:11:18 +0900

    feat: signToken: access, refresh 토큰 생성 함수 구현

--- BackEnd/src/auth/auth.service.ts
+++ BackEnd/src/auth/auth.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ProfileModel } from 'src/profiles/entities/profiles.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly jwtService: JwtService,
+    ){}
+
+    signToken(publicId: Pick<ProfileModel, 'publicId'>, isRefreshToken: boolean) {
+        //payload에는 sub -> id가 들어감 (사용자를 고유하게 식별하는데 사용), type (access token, refresh token)
+        const payload = {
+            sub: publicId,
+            type: isRefreshToken ? 'refresh' : 'access',
+        };
+
+        return this.jwtService.sign(payload, {
+            secret: process.env.JWT_SECRET,
+            expiresIn: isRefreshToken ? 86400 : 3600,
+        });
+    }
+}
