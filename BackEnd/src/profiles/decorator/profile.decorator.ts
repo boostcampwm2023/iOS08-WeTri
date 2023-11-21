9be019508ff6b9749ecd6533e8f9fc2a6f3a09e6
신정용 <shinjy0211@gmail.com>
2023-11-21 15:04:21 +0900

    feat: createExerciseLog 함수 구현 -> 유저가 운동 후 기록

---
+++ BackEnd/src/profiles/decorator/profile.decorator.ts
@@ -0,0 +1,13 @@
+import { ExecutionContext, InternalServerErrorException, createParamDecorator } from "@nestjs/common";
+
+export const Profile = createParamDecorator((data, context: ExecutionContext) => {
+    const req = context.switchToHttp().getRequest();
+
+    const profile = req.profile;
+
+    if(!profile) {
+        throw new InternalServerErrorException('AccessTokenGuard를 사용하지 않았습니다.');
+    }
+
+    return profile;
+});
--- BackEnd/src/records/records.module.ts
+++ BackEnd/src/records/records.module.ts
@@ -3,9 +3,14 @@ import { RecordsService } from './records.service';
 import { RecordsController } from './records.controller';
 import { AuthModule } from 'src/auth/auth.module';
 import { ProfilesModule } from 'src/profiles/profiles.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RecordModel } from './entities/records.entity';
 
 @Module({
-  imports: [AuthModule, ProfilesModule],
+  imports: [
+    TypeOrmModule.forFeature([RecordModel]),
+    AuthModule, 
+    ProfilesModule],
   controllers: [RecordsController],
   providers: [RecordsService],
 })
