66dedad387f39a466a7a48563a52b43b63e9c0e6
JongPyoAhn <whdvy95@gmail.com>
2023-11-22 17:07:58 +0900

    docs: 주석제거

--- iOS/Projects/Features/Record/Sources/Domain/UseCases/Protocol/RecordUpdateUsecase.swift
+++ iOS/Projects/Features/Record/Sources/Domain/UseCases/Protocol/RecordUpdateUsecase.swift
@@ -12,9 +12,6 @@ import Foundation
 // MARK: - RecordUpdateUsecase
 
 protocol RecordUpdateUsecase {
-  /// Usecase에서 Repository로 넘기기전에 String형태의 날짜로 바꿔서 보내주기.
-  /// yyyy-mm-dd 형식
-  /// 따로 Usecase에서 처리할 비즈니스로직 없으면 Repository에서 받아온 데이터 그대로 넘기기.
   func execute(calendarData: CalendarData) -> AnyPublisher<[Record], Error>
 }
 
