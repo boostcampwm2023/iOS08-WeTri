5c6ba7097923a63b6655cbfac9b51dd74d2598c8
wonholim <kds0034@gmail.com>
2023-11-22 21:06:38 +0900

    fix: 외존성 주입을 생성자로 사용

--- BackEnd/src/lives/lives.gateway.ts
+++ BackEnd/src/lives/lives.gateway.ts
@@ -11,7 +11,7 @@ import {LoggerService} from "@nestjs/common";
 @WebSocketGateway(3003)
 export class LiveGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: WebSocket.Server;
-  private readonly logger: LoggerService;
+  constructor(private readonly logger: LoggerService) {}
 
   @SubscribeMessage('events')
   onEvent(client: WebSocket, data: any): void {
