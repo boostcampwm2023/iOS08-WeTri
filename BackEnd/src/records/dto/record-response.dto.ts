1976bce50cb71c82c3e358d0a3c58bd94007aa1f
신정용 <89844585+sjy982@users.noreply.github.com>
2023-11-23 19:49:23 +0900

    [GWL-83] Record Swagger 리팩토링 (#114)
    * refacotr: dto 리팩토링
    
    * chore: format 적용

---
+++ BackEnd/src/records/dto/record-response.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { SuccessResDto } from 'src/common/dto/SuccessRes.dto';
+import { RecordModel } from '../entities/records.entity';
+
+class GetRecord extends PickType(RecordModel, [
+  'id',
+  'workout',
+  'profile',
+  'workoutTime',
+  'distance',
+  'calorie',
+  'avgHeartRate',
+  'minHeartRate',
+  'maxHeartRate',
+  'createdAt',
+]) {}
+
+export class CreateRecordResDto extends SuccessResDto {
+  @ApiProperty({ example: 1, description: '운동 기록 레코드 ID' })
+  recordId: number;
+}
+
+export class GetUsersRecordsResDto extends SuccessResDto {
+  @ApiProperty({ type: () => [GetRecord] })
+  data: GetRecord[];
+}
+
+export class GetRecordResDto extends SuccessResDto {
+  @ApiProperty({ type: () => GetRecord })
+  data: GetRecord;
+}
--- BackEnd/src/records/entities/records.entity.ts
+++ BackEnd/src/records/entities/records.entity.ts
@@ -10,47 +10,69 @@ import {
 import { PostModel } from '../../posts/entities/posts.entity';
 import { ProfileModel } from '../../profiles/entities/profiles.entity';
 import { IsNumber, IsString } from 'class-validator';
-import {ApiProperty} from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
 @Entity()
 export class RecordModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ApiProperty({ example: 'biking', description: '선택한 운동 종료' })
+  @ApiProperty({ example: '자전거', description: '선택한 운동 종료' })
   @Column()
   @IsString()
   workout: string;
 
-  @ApiProperty({ example: '6000000', description: '운동을 한 시간 (초로 환산)' })
+  @ApiProperty({
+    example: '6000000',
+    description: '운동을 한 시간 (초로 환산)',
+  })
   @Column()
   @IsNumber()
   workoutTime: number;
 
-  @ApiProperty( {example: '100000', description: '운동을 한 거리 (미터로 환산)'} )
+  @ApiProperty({
+    example: '100000',
+    description: '운동을 한 거리 (미터로 환산)',
+  })
   @Column()
   @IsNumber()
   distance: number;
 
-  @ApiProperty( {example: '360', description: '운동에 소모한 칼로리 (kcal 기준)'} )
+  @ApiProperty({
+    example: '360',
+    description: '운동에 소모한 칼로리 (kcal 기준)',
+  })
   @Column()
   @IsNumber()
   calorie: number;
 
-  @ApiProperty({example: '60', description: '운동 중 평균 심박수 (평균 기준)'})
+  @ApiProperty({
+    example: '60',
+    description: '운동 중 평균 심박수 (평균 기준)',
+  })
   @Column({ nullable: true })
   @IsNumber()
   avgHeartRate: number;
 
-  @ApiProperty( {example: '120', description: '운동 중 최대 심박수 (최대 기준)'} )
+  @ApiProperty({
+    example: '120',
+    description: '운동 중 최대 심박수 (최대 기준)',
+  })
   @Column({ nullable: true })
   @IsNumber()
   minHeartRate: number;
 
-  @ApiProperty( {example: '180', description: '운동 중 최소 심박수 (최소 기준)'} )
+  @ApiProperty({
+    example: '180',
+    description: '운동 중 최소 심박수 (최소 기준)',
+  })
   @Column({ nullable: true })
   @IsNumber()
   maxHeartRate: number;
 
+  @ApiProperty({
+    example: 'YYYY-MM-DD hh:mm:ss',
+    description: '운동 기록 생성 날짜',
+  })
   @CreateDateColumn()
   createdAt: Date;
 
