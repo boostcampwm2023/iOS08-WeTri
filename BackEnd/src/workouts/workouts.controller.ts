649d95113f88265f6e206bf64e74f88c2e209a1b
wonholim <kds0034@gmail.com>
2023-11-23 20:42:15 +0900

    feat: workouts 컨트롤러 getAllWorkout 기능 구현

--- BackEnd/src/workouts/workouts.controller.ts
+++ BackEnd/src/workouts/workouts.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { WorkoutsService } from './workouts.service';
-import {ApiOperation, ApiTags} from "@nestjs/swagger";
+import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {WorkoutResDto} from "./dto/workout-response.dto";
 
 @ApiTags('운동 종류 API')
 @Controller('workouts')
@@ -8,7 +9,12 @@ export class WorkoutsController {
   constructor(private readonly workoutsService: WorkoutsService) {}
   @Get()
   @ApiOperation({summary: '모든 운동 종류 조회'})
-  findAll() {
+  @ApiResponse({
+    status: 200,
+    description: '성공',
+    type: WorkoutResDto,
+  })
+  getAllWorkout() {
     return this.workoutsService.findAll();
   }
 }
