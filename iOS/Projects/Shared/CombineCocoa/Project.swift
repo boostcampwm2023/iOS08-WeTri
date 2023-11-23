cb5ee1211f0437023e288763e9ec5a57e3845327
SeungHyun Hong <whi7ehyun@gmail.com>
2023-11-23 09:54:36 +0900

    test: bind(to:) unit test 추가

--- iOS/Projects/Shared/CombineCocoa/Project.swift
+++ iOS/Projects/Shared/CombineCocoa/Project.swift
@@ -3,5 +3,9 @@ import ProjectDescriptionHelpers
 
 let project = Project.makeModule(
   name: "CombineCocoa",
-  targets: .custom(name: "CombineCocoa", product: .framework)
+  targets: .custom(
+    name: "CombineCocoa",
+    product: .framework,
+    testingOptions: [.unitTest]
+  )
 )
