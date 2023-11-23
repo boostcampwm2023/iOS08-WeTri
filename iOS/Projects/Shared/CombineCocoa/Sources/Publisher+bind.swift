10babbb3ab61f2d5b90473feb999ba00aa764764
SeungHyun Hong <whi7ehyun@gmail.com>
2023-11-23 16:15:54 +0900

    [GWL-74] CocoaCombine bind(to:) 추가 (#103)
    * feat: bind Method 구현
    
    * test: bind(to:) unit test 추가
    
    * fix: Test코드에서 Just Publisher 호출 위치 변경

---
+++ iOS/Projects/Shared/CombineCocoa/Sources/Publisher+bind.swift
@@ -0,0 +1,17 @@
+//
+//  Publisher+bind.swift
+//  CombineCocoa
+//
+//  Created by 홍승현 on 11/23/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import Combine
+
+public extension Publisher where Failure == Never {
+  func bind<S>(to subject: S) -> AnyCancellable where S: Subject, S.Output == Output, S.Failure == Failure {
+    return sink { value in
+      subject.send(value)
+    }
+  }
+}
---
+++ iOS/Projects/Shared/CombineCocoa/Tests/Publisher+bindTests.swift
@@ -0,0 +1,68 @@
+//
+//  Publisher+bindTests.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 홍승현 on 11/23/23.
+//
+
+@testable import CombineCocoa
+
+import Combine
+import XCTest
+
+final class Publisher_bindTests: XCTestCase {
+  private var subscriptions: Set<AnyCancellable> = []
+  private let publisher: PassthroughSubject<Void, Never> = .init()
+  private var sut: AnyPublisher<Void, Never>?
+
+  override func tearDown() {
+    for subscription in subscriptions {
+      subscription.cancel()
+    }
+    subscriptions.removeAll()
+  }
+
+  func testBindToPassthroughSubject() {
+    // assign
+    let expectation = XCTestExpectation(description: "PassthroughSubject should receive value")
+    let passthroughSubject = PassthroughSubject<String, Never>()
+    let testValue = "Test"
+
+    // act & assert
+
+    passthroughSubject
+      .sink { receivedValue in
+        XCTAssertEqual(receivedValue, testValue)
+        expectation.fulfill()
+      }
+      .store(in: &subscriptions)
+
+    Just(testValue)
+      .bind(to: passthroughSubject)
+      .store(in: &subscriptions)
+
+    wait(for: [expectation], timeout: 1.0)
+  }
+
+  func testBindToCurrentValueSubject() {
+    // assign
+    let expectation = XCTestExpectation(description: "CurrentValueSubject should receive value")
+    let currentValueSubject = CurrentValueSubject<String, Never>("Initial Value")
+    let testValue = "Test"
+
+    // act
+    Just(testValue)
+      .bind(to: currentValueSubject)
+      .store(in: &subscriptions)
+
+    // assert
+    currentValueSubject
+      .sink { receivedValue in
+        XCTAssertEqual(receivedValue, testValue)
+        expectation.fulfill()
+      }
+      .store(in: &subscriptions)
+
+    wait(for: [expectation], timeout: 1.0)
+  }
+}
