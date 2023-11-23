bd145d3145bc611c4cf6df75b8da84ccdc90a1e2
SeungHyun Hong <whi7ehyun@gmail.com>
2023-11-23 20:07:58 +0900

    add: WorkoutSessionContainerScene 추가

---
+++ iOS/Projects/Features/Record/Sources/Presentation/WorkoutSessionGroupScene/WorkoutSessionContainerViewController.swift
@@ -0,0 +1,84 @@
+//
+//  WorkoutSessionContainerViewController.swift
+//  RecordFeature
+//
+//  Created by 홍승현 on 11/23/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import Combine
+import DesignSystem
+import UIKit
+
+// MARK: - WorkoutSessionContainerViewController
+
+final class WorkoutSessionContainerViewController: UIViewController {
+  // MARK: Properties
+
+  private let viewModel: WorkoutSessionContainerViewModelRepresentable
+
+  private var subscriptions: Set<AnyCancellable> = []
+
+  // MARK: UI Components
+
+  private let button: UIButton = .init(configuration: .mainEnabled(title: "test button"))
+
+  // MARK: Initializations
+
+  init(viewModel: WorkoutSessionContainerViewModelRepresentable) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  @available(*, unavailable)
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: Life Cycles
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupLayouts()
+    setupConstraints()
+    setupStyles()
+    bind()
+  }
+
+  // MARK: Configuration
+
+  private func setupLayouts() {
+    view.addSubview(button)
+  }
+
+  private func setupConstraints() {
+    button.translatesAutoresizingMaskIntoConstraints = false
+
+    NSLayoutConstraint.activate(
+      [
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      ]
+    )
+  }
+
+  private func setupStyles() {
+    view.backgroundColor = DesignSystemColor.primaryBackground
+  }
+
+  private func bind() {
+    let output = viewModel.transform(input: .init())
+    output.sink { state in
+      switch state {
+      case .idle:
+        break
+      }
+    }
+    .store(in: &subscriptions)
+  }
+}
+
+@available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, xrOS 1.0, *)
+#Preview {
+  WorkoutSessionContainerViewController(viewModel: WorkoutSessionContainerViewModel())
+}
