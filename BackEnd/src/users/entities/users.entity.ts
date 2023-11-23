6469e554b921d9506fd4a96fa55d1266b9101140
신정용 <shinjy0211@gmail.com>
2023-11-23 14:23:07 +0900

    feat: swagger 작업

--- BackEnd/src/users/entities/users.entity.ts
+++ BackEnd/src/users/entities/users.entity.ts
@@ -7,17 +7,20 @@ import {
 } from 'typeorm';
 import { ProfileModel } from '../../profiles/entities/profiles.entity';
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 @Entity()
 export class UserModel {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: "testid132", description: 'user의 id를 뜻합니다. (apple이면 apple id)' })
   @Column()
   @IsString({
     message: 'userId는 string 타입을 입력해야합니다.',
   })
   userId: string;
 
+  @ApiProperty({ example: "apple", description: "userId의 플랫폼입니다." })
   @Column()
   @IsString({
     message: 'provider는 string 타입을 입력해야합니다.',
