1c88f20f812b4e25c70d092cec917816e5c6716e
wonholim <kds0034@gmail.com>
2023-11-25 20:21:50 +0900

    add: event 폴더 생성

---
+++ BackEnd/src/live-workouts/events/events.gateway.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EventsGateway } from './events.gateway';
+import { EventsService } from './events.service';
+
+describe('EventsGateway', () => {
+  let gateway: EventsGateway;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [EventsGateway, EventsService],
+    }).compile();
+
+    gateway = module.get<EventsGateway>(EventsGateway);
+  });
+
+  it('should be defined', () => {
+    expect(gateway).toBeDefined();
+  });
+});
