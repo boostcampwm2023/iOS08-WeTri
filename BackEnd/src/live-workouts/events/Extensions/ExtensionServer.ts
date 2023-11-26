1c658645f2c286f93d1d6dc9fc4136e8fcf58e31
신정용 <shinjy0211@gmail.com>
2023-11-26 20:48:16 +0900

    feat: server joinRoom 구현

---
+++ BackEnd/src/live-workouts/events/Extensions/ExtensionServer.ts
@@ -0,0 +1,16 @@
+import { Server , WebSocket } from 'ws';
+
+export class ExtensionWebSocketServer {
+    rooms: Map<string, Set<WebSocket>> = new Map();
+
+    constructor(server: Server) {
+        server.rooms = this.rooms;
+    }
+
+    joinRoom(client: WebSocket, roomName: string) {
+        if(!this.rooms.has(roomName)) {
+            this.rooms.set(roomName, new Set());
+        }
+        this.rooms.get(roomName).add(client);
+    }
+}
