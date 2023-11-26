acdf885fa66867ab7a1edc3c72d2af3840bcd1d9
wonholim <kds0034@gmail.com>
2023-11-27 00:18:31 +0900

    chore: DTO 생성 및 Swagger 적용

--- BackEnd/src/live-workouts/matches/dto/create-match.dto.ts
+++ BackEnd/src/live-workouts/matches/dto/create-match.dto.ts
@@ -1,1 +1,10 @@
-export class CreateMatchDto {}
+import { IsNotEmpty, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { SuccessResDto } from '../../../common/dto/SuccessRes.dto';
+
+export class CreateMatchDto {
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({ example: 1 })
+  workoutId: number;
+}
