87f4fac94f21a5476cc0a19f0f4703148b250be7
wonholim <kds0034@gmail.com>
2023-11-27 20:26:31 +0900

    chore: 서비스 코드 수정에 따른 테스트 코드 수정

--- BackEnd/src/live-workouts/matches/matches.service.spec.ts
+++ BackEnd/src/live-workouts/matches/matches.service.spec.ts
@@ -71,7 +71,7 @@ describe('MatchesService', () => {
 
     it('매칭을 취소하면, maching:1에 있는 value는 삭제가 되어야 한다.', async () => {
       await service.cancelMatch(profile, createMatchDto);
-      expect(lrem).toHaveBeenCalledWith(`matching:1`, 0, 'TestUser');
+      expect(lrem).toHaveBeenCalledWith(`matching:1`, 0, JSON.stringify(profile));
     });
 
     it('랜덤 매칭 2명일 때, 기다린 시간이 60초 그리고 레디스가 잘 작동하는지 테스트한다.', async () => {
@@ -157,13 +157,13 @@ describe('MatchesService', () => {
 
   describe('initMatch 메서드 검증', () => {
     it('닉네임과 운동 종류가 주어질 때, 실제로 리스트에서 제거 및 매칭에서 제거되는지 확인한다.', async () => {
-      const nickname = 'TestUser';
+      const profile = { nickname: 'TestUser' } as Profile;
       const workoutId = 1;
 
-      await service['initMatch'](nickname, workoutId);
+      await service['initMatch'](profile, workoutId);
 
-      expect(lrem).toHaveBeenCalledWith(`matching:${workoutId}`, 0, nickname);
-      expect(del).toHaveBeenCalledWith(`userMatch:${nickname}`);
+      expect(lrem).toHaveBeenCalledWith(`matching:${workoutId}`, 0, JSON.stringify(profile));
+      expect(del).toHaveBeenCalledWith(`userMatch:${profile.nickname}`);
     });
   });
 });
