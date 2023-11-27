9a616596987f082ba19ee026c8fa75e4abded754
SeungHyun Hong <whi7ehyun@gmail.com>
2023-11-27 09:38:39 +0900

    feat: MockEndPoint으로 분리 Session test code제거

--- iOS/Projects/Core/Network/Tests/EndPointTests.swift
+++ iOS/Projects/Core/Network/Tests/EndPointTests.swift
@@ -9,25 +9,10 @@
 import XCTest
 
 final class EndPointTests: XCTestCase {
-  struct TestEndPoint: TNEndPoint {
-    var baseURL: String = "base"
-    var path: String = "path"
-    var method: TNMethod = .get
-    var query: Encodable? = nil
-    var body: Encodable? = nil
-    var headers: TNHeaders = .init(headers: [
-      .init(key: "First", value: "First"),
-    ])
-  }
-
-  var sut: TNEndPoint! = nil
-
-  let provider = TNProvider<TestEndPoint>(session: URLSession.shared)
-
-  func temp() async throws {}
+  private var sut: TNEndPoint?
 
   override func setUp() {
-    sut = TestEndPoint()
+    sut = MockEndPoint()
   }
 
   override func tearDown() {
@@ -35,10 +20,10 @@ final class EndPointTests: XCTestCase {
   }
 
   func test_Base포함Path미포함_urlRequest를생성_URL확인() throws {
-    sut = TestEndPoint(baseURL: "base", path: "")
+    sut = MockEndPoint(baseURL: "base", path: "")
 
     // Act
-    let targetURL = try sut.request().url
+    let targetURL = try sut?.request().url
 
     // Assert
     XCTAssertNotNil(targetURL)
@@ -47,7 +32,7 @@ final class EndPointTests: XCTestCase {
 
   func test_Base와Path를포함_urlRequest를생성_성공() throws {
     // Act
-    let targetURL = try sut.request().url
+    let targetURL = try sut?.request().url
 
     // Assert
     XCTAssertNotNil(targetURL)
@@ -55,9 +40,9 @@ final class EndPointTests: XCTestCase {
   }
 
   func test_Query() throws {
-    sut = TestEndPoint(query: ["Some": "Time"])
+    sut = MockEndPoint(query: ["Some": "Time"])
     // Act
-    let targetURL = try sut.request().url
+    let targetURL = try sut?.request().url
 
     // Assert
     XCTAssertNotNil(targetURL)
@@ -65,9 +50,9 @@ final class EndPointTests: XCTestCase {
   }
 
   func test_QueryIntValue() throws {
-    sut = TestEndPoint(query: ["Int": "3"])
+    sut = MockEndPoint(query: ["Int": "3"])
     // Act
-    let targetURL = try sut.request().url
+    let targetURL = try sut?.request().url
 
     // Assert
     XCTAssertNotNil(targetURL)
@@ -76,17 +61,17 @@ final class EndPointTests: XCTestCase {
 
   func test_Body에String을넣고생성하고_request를생성했을때_body값이주입한String과똑같다() throws {
     let bodyString = "바디 스트링"
-    sut = TestEndPoint(body: bodyString)
+    sut = MockEndPoint(body: bodyString)
 
-    let body = try sut.request().httpBody!
+    let body = try sut!.request().httpBody!
 
     XCTAssertEqual(bodyString, try JSONDecoder().decode(String.self, from: body))
   }
 
   func test_Body에nil을넣고_request를생성했을때_body의값이nil이적재된다() throws {
-    sut = TestEndPoint(body: nil)
+    sut = MockEndPoint(body: nil)
 
-    let body = try sut.request().httpBody
+    let body = try sut?.request().httpBody
 
     XCTAssertNil(body)
   }
@@ -94,38 +79,11 @@ final class EndPointTests: XCTestCase {
   func test_HTTPHeader에값을넣고_request을생성하면_넣은Header값과똑같다() throws {
     let headers: TNHeaders = [
       "Header": "1",
-      "Headerable": "2"
+      "Headerable": "2",
     ]
 
-    sut = TestEndPoint(headers: headers)
-    let requestHeader = try sut.request().allHTTPHeaderFields
+    sut = MockEndPoint(headers: headers)
+    let requestHeader = try sut?.request().allHTTPHeaderFields
     XCTAssertEqual(headers.dictionary, requestHeader)
-  }
-
-  func test_Mock확인() throws {
-    let testData = "hihi".data(using: .utf8)!
-    let mockSession = MockURLSession(mockData: testData)
-    let provider = TNProvider<TestEndPoint>(session: mockSession)
-    let targetSut = TestEndPoint()
-
-    let expectation = XCTestExpectation(description: "API호출 완료 Expectaion")
-    var providedData = Data()
-    try provider.request(targetSut) { data, _, _ in
-      providedData = data ?? Data()
-      expectation.fulfill()
-    }
-    wait(for: [expectation], timeout: 3)
-
-    XCTAssertEqual(testData, providedData)
-  }
-
-  func test_Mock확인2() async throws {
-    let testData = "안녕하세요".data(using: .utf8)!
-    let mockSession = MockURLSession(mockData: testData)
-    let provider = TNProvider<TestEndPoint>(session: mockSession)
-    let targetSut = TestEndPoint()
-    let provideData = try await provider.request(targetSut)
-
-    XCTAssertEqual(testData, provideData)
   }
 }
