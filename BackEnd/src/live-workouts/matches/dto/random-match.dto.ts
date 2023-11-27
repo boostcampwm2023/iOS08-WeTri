b0f6b8e93caf34b57ae81f7a5e7a454324828ad9
Vardy <kds0034@gmail.com>
2023-11-27 20:34:47 +0900

    [GWL-121] 매칭 API 구현 (#135)
    * chore: Redis 의존성 추가
    
    * chore: Redis 의존성 추가 10.2.1 버전
    
    * add: 레디스 config 파일 생성
    
    * chore: 레디스 imports에 추가
    
    * chore: 매칭 관련 Constant 관리 파일
    
    * feat: Random 매칭에 필요한 DTO 작성과 Swagger 작성
    
    * chore: redis config 작성을 공식문서와 동일하게 작성
    
    * chore: DTO 생성 및 Swagger 적용
    
    * delete: entity 삭제
    
    * chore: redis Config 클래스 작성
    
    * delete: 매치 업데이트 제거
    
    * feat: 매칭을 시작하는 API, 중단하는 API, 잡혔는지 Short 폴링하는 API 작성
    
    * test: 테스트 코드 작성!
    
    * chore: 의존성 수정, 및 곧 대규모 삭제 예정
    
    * chore: Swagger 재설정
    
    * chore: lint 적용
    
    * delete: 컨트롤러 spec.ts 삭제
    
    * ci: 의존성 설치 수정
    
    * delete: 외부에 생긴 package-lock 제거
    
    * chore: 의존성 수정 및 설치
    
    * delete: 타입 오류로 인한 서버 재시작 문제로 삭제..
    
    * chore: event 클래스 수정
    
    * chore: redis 비밀번호 제거
    
    * refactor: redis에서 profile 삭제가 아닌 nickname 삭제 로직을 수정
    
    * chore: auth에서 JWT 관련된 부분을 env 변수로 수정
    
    * chore: 서비스 코드 수정에 따른 테스트 코드 수정

---
+++ BackEnd/src/live-workouts/matches/dto/random-match.dto.ts
@@ -0,0 +1,69 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+import { Profile } from '../../../profiles/entities/profiles.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { SuccessResDto } from '../../../common/dto/SuccessRes.dto';
+
+export class RandomMatchDto {
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({ example: 1 })
+  workoutId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({ example: 60 })
+  waitingTime: number;
+}
+
+export class RandomMatch {
+  @IsBoolean()
+  @IsNotEmpty()
+  @ApiProperty({ example: true })
+  matched: boolean;
+
+  @IsString()
+  @ApiProperty({ example: 'yyyy-mm-dd hh:mm:ss' })
+  liveWorkoutStartTime?: string;
+
+  @IsString()
+  @ApiProperty({ example: 'uuid' })
+  matchId?: string;
+
+  @ApiProperty({
+    example: [
+      {
+        nickname: 'nickname',
+        publicId: 'profileImage',
+        etc: 'etc',
+      },
+      {
+        nickname: 'nickname',
+        publicId: 'profileImage',
+        etc: 'etc',
+      },
+    ],
+  })
+  peers?: Profile[];
+}
+
+export class RandomMatchResDto extends SuccessResDto {
+  @ApiProperty({ type: () => RandomMatch })
+  data: RandomMatch;
+}
+
+export const randomNoMatchingResDto = () => {
+  return {
+    example: {
+      code: null,
+      errorMessage: null,
+      data: {
+        matched: false,
+      },
+    },
+  };
+};
--- BackEnd/src/live-workouts/matches/matches.module.ts
+++ BackEnd/src/live-workouts/matches/matches.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MatchesService } from './matches.service';
 import { MatchesController } from './matches.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../../users/users.module';
+import { ProfilesModule } from '../../profiles/profiles.module';
+import { AuthService } from '../../auth/auth.service';
+import { RedisModule } from '@songkeys/nestjs-redis';
+import { RedisConfigService } from '../../config/redis.config';
 
 @Module({
+  imports: [
+    RedisModule.forRootAsync({
+      useClass: RedisConfigService,
+    }),
+    JwtModule.register({}),
+    UsersModule,
+    ProfilesModule,
+  ],
   controllers: [MatchesController],
-  providers: [MatchesService],
+  providers: [MatchesService, AuthService],
 })
 export class MatchesModule {}
