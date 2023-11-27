2590153977714528c28d07c0030e9672bae15318
SeungHyun Hong <whi7ehyun@gmail.com>
2023-11-27 10:45:51 +0900

    fix: Fix typo

--- iOS/Projects/Core/Network/Sources/TNError.swift
+++ iOS/Projects/Core/Network/Sources/TNError.swift
@@ -21,13 +21,13 @@ public enum TNError: LocalizedError {
     case .invalidURL:
       return "URL이 잘못되었습니다."
     case .clientError:
-      return "Client Response Errror가 발생하였습니다."
+      return "Client Response Error가 발생하였습니다."
     case .redirectError:
       return "Client는 요청 완료를 위해 리다이렉션과 같은 작업을 수행해야 합니다."
     case .serverError:
       return "Server Error가 발생하였습니다."
     case .unknownError:
-      return "UnkownError가 발생하였습니다."
+      return "UnknownError가 발생하였습니다."
     case .httpResponseDownCastingError:
       return "HTTPResponse를 다운캐스팅 할 수 없습니다. 요청하는 URL주소를 다시 확인하거나, Mock Response를 확인해 주세요"
     }
