c63707c08dc5ce4e47d8dde205204e8d8fd833fc
SeungHyun Hong <whi7ehyun@gmail.com>
2023-11-29 08:51:37 +0900

    feat: HealthKit으로 받아온 데이터를 UI에 렌더링

--- iOS/Projects/Features/Record/Sources/Presentation/WorkoutSessionGroupScene/SessionScene/WorkoutSessionViewModel.swift
+++ iOS/Projects/Features/Record/Sources/Presentation/WorkoutSessionGroupScene/SessionScene/WorkoutSessionViewModel.swift
@@ -18,6 +18,9 @@ public typealias WorkoutSessionViewModelOutput = AnyPublisher<WorkoutSessionStat
 
 public enum WorkoutSessionState {
   case idle
+  // FIXME: API Model로 바꿔야합니다.
+  case connectHealthData(distance: Double, calories: Double, heartRate: Double)
+  case alert(Error)
 }
 
 // MARK: - WorkoutSessionViewModelRepresentable
@@ -50,6 +53,10 @@ extension WorkoutSessionViewModel: WorkoutSessionViewModelRepresentable {
 
     let initialState: WorkoutSessionViewModelOutput = Just(.idle).eraseToAnyPublisher()
 
-    return initialState
+    let healthData = useCase.healthPublisher
+      .map(WorkoutSessionState.connectHealthData)
+      .catch { Just(.alert($0)) }
+
+    return initialState.merge(with: healthData).eraseToAnyPublisher()
   }
 }
