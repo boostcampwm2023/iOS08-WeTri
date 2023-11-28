4ebf3ca06972aa2cb4a094fefdb9712b1e0c2819
JongPyoAhn <whdvy95@gmail.com>
2023-11-28 23:51:15 +0900

    add: LoginFeature 구조

---
+++ iOS/Projects/Features/Login/Sources/Domain/Interfaces/Repositories/AuthorizationRepositoryRepresentable.swift
@@ -0,0 +1,11 @@
+//
+//  AuthorizationRepositoryRepresentable.swift
+//  LoginFeature
+//
+//  Created by 안종표 on 11/28/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import Foundation
+
+protocol AuthorizationRepositoryRepresentable {}
---
+++ iOS/Projects/Features/Login/Sources/Domain/UseCases/AuthorizeUseCase.swift
@@ -0,0 +1,11 @@
+//
+//  AuthorizeUseCase.swift
+//  LoginFeature
+//
+//  Created by 안종표 on 11/28/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import Foundation
+
+final class AuthorizeUseCase: AuthorizeUseCaseRepresentable {}
