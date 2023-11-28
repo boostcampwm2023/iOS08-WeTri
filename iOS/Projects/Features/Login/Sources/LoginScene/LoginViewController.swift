e1f42082fe99fb30c15c4650e1dc4fcbcbd8fd10
JongPyoAhn <whdvy95@gmail.com>
2023-11-28 22:57:33 +0900

    add: LoginViewController,ViewModel

---
+++ iOS/Projects/Features/Login/Sources/LoginScene/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  LoginFeature
+//
+//  Created by 안종표 on 11/28/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import UIKit
+
+public final class LoginViewController: UIViewController {
+  
+  private let viewModel: LoginViewModelRepresentable
+
+  public init(viewModel: LoginViewModelRepresentable) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  @available(*, unavailable)
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override public func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  
+}
