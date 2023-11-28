69222f5954a8787ee47b0aca842c81caf3386975
JongPyoAhn <whdvy95@gmail.com>
2023-11-28 21:48:20 +0900

    chore: LoginFeature모듈 생성

---
+++ iOS/Projects/Features/Login/Project.swift
@@ -0,0 +1,13 @@
+import DependencyPlugin
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.makeModule(
+  name: "LoginFeature",
+  targets: .feature(
+    .login,
+    testingOptions: [.unitTest],
+    dependencies: [],
+    testDependencies: []
+  )
+)
