07f208f1f9abe90750abfc17f563e6dccd4a9c44
SeungHyun Hong <whi7ehyun@gmail.com>
2023-11-29 14:23:15 +0900

    add: WebSocketTaskProtocol 추가
    - WebSocketTask의 모든 사용을 방지하고, 오로지 send, receive만 처리할 수 있도록 추상화했습니다.

---
+++ iOS/Projects/Core/Network/Sources/WebSocketTaskProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  WebSocketTaskProtocol.swift
+//  Trinet
+//
+//  Created by 홍승현 on 11/29/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - WebSocketTaskProtocol
+
+public protocol WebSocketTaskProtocol {
+  func send(_ message: URLSessionWebSocketTask.Message) async throws
+
+  func receive() async throws -> URLSessionWebSocketTask.Message
+}
+
+// MARK: - URLSessionWebSocketTask + WebSocketTaskProtocol
+
+extension URLSessionWebSocketTask: WebSocketTaskProtocol {}
