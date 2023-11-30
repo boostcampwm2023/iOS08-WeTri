5c8ba4f5d9778e73dd2d46c36bd9d26332038763
wonholim <kds0034@gmail.com>
2023-11-30 16:35:22 +0900

    feat: NCP Object Storage에 접근해 사진을 업로드하는 기능 구현
    uuid로 이미지 명을 정하며, 리턴시에는 CDN 주소를 리턴한다.

--- BackEnd/src/images/images.service.ts
+++ BackEnd/src/images/images.service.ts
@@ -1,13 +1,53 @@
 import {Injectable} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
+import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ImagesService {
+    private readonly s3Client = new S3Client({
+        endpoint: this.configService.getOrThrow('NCP_ENDPOINT'),
+        region: this.configService.getOrThrow('NCP_REGION'),
+        credentials: {
+            accessKeyId: this.configService.getOrThrow('NCP_ACCESS_KEY'),
+            secretAccessKey: this.configService.getOrThrow('NCP_SECRET_KEY'),
+        },
+    })
+    constructor(private readonly configService: ConfigService) {}
 
-    async uploadImage() {
-        return Promise.resolve(undefined);
+    async uploadImage(images: Express.Multer.File[]) {
+        const imageURL = [];
+        for (const image of images) {
+            const imageBuffer = image.buffer;
+            const imageExtension = this.getExtension(image.originalname);
+            if (!this.isGreenEye(imageBuffer)) {
+
+            }
+
+            const imageId = `${uuidv4()}.${imageExtension}`;
+            await this.s3Client.send(
+                new PutObjectCommand({
+                    Bucket: this.configService.getOrThrow('NCP_BUCKET_NAME'),
+                    Key: imageId,
+                    Body: imageBuffer,
+                    ContentType: image.mimetype,
+                })
+            );
+
+            imageURL.push({
+                imageName: image.originalname,
+                imageUrl: this.configService.getOrThrow('NCP_CDN_URL')
+                + imageId
+            });
+
+        }
+        return imageURL;
     }
 
-    async uploadMultipleImages() {
-        return Promise.resolve(undefined);
+    private getExtension(imageName: string) {
+        return imageName.split('.').pop();
+    }
+    private isGreenEye(imageBuffer: Buffer) {
+        return false;
     }
 }
