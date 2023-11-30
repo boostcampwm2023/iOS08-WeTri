f188e796ba17699f1f600da5fd0b74ede9840094
Vardy <kds0034@gmail.com>
2023-11-30 22:47:19 +0900

    fix: UseGuard 모듈 의존성 imports 추가 (#195)

--- BackEnd/src/images/images.module.ts
+++ BackEnd/src/images/images.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ImagesController } from './images.controller';
 import { ImagesService } from './images.service';
+import {AuthModule} from "../auth/auth.module";
+import {ProfilesModule} from "../profiles/profiles.module";
 
 @Module({
+  imports: [AuthModule, ProfilesModule],
   controllers: [ImagesController],
   providers: [ImagesService],
 })
