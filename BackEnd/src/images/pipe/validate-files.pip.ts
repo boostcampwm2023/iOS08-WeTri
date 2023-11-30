49613253fa4ac3612da60073b702f9b3753e3f9e
wonholim <kds0034@gmail.com>
2023-11-30 16:32:41 +0900

    feat: 여러개의 파일 업로드에서 파일의 확장명과 파일의 크기를 검증하는 커스텀 파이프 클래스 구현

---
+++ BackEnd/src/images/pipe/validate-files.pip.ts
@@ -0,0 +1,30 @@
+import { Injectable, BadRequestException, PipeTransform } from '@nestjs/common';
+import {FileUploadOptions} from "../interface/images.interface";
+
+@Injectable()
+export class ValidateFilesPipe implements PipeTransform {
+    constructor(private options: FileUploadOptions) {}
+
+    transform(files: Express.Multer.File[]): Express.Multer.File[] {
+        files.forEach(file => {
+            this.validateFileSize(file);
+            this.validateFileType(file);
+        });
+
+        return files;
+    }
+
+    private validateFileSize(file: Express.Multer.File): void {
+        const maxSize = this.options.maxSize;
+        if (file.size > maxSize) {
+            throw new BadRequestException(`File size too large: ${file.originalname}`);
+        }
+    }
+
+    private validateFileType(file: Express.Multer.File): void {
+        const allowedTypes = this.options.fileType;
+        if (!allowedTypes.includes(file.mimetype)) {
+            throw new BadRequestException(`Invalid file type: ${file.originalname}`);
+        }
+    }
+}
