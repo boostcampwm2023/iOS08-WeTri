1b25c087ef9f4a06a8bedae77217f9daebd8205e
SeungHyun Hong <whi7ehyun@gmail.com>
2023-12-03 21:48:26 +0900

    docs: reissueToken DocC 추가

--- iOS/Projects/Features/Splash/Sources/Domain/UseCases/SplashUseCase.swift
+++ iOS/Projects/Features/Splash/Sources/Domain/UseCases/SplashUseCase.swift
@@ -19,7 +19,14 @@ public struct SplashUseCase: SplashUseCaseRepresentable {
   public init(repository: SplashTokenRepositoryRepresentable) {
     self.repository = repository
   }
-
+  
+  /// 토큰을 재발급받습니다.
+  /// - Returns: 토큰 재발급시 True, 실패시 False를 리턴하는 Publisher
+  ///
+  /// - Description:
+  ///   repository에서 `refresh token` 재발급과 `access token` 재발급을 요청합니다.
+  ///   모든 토큰을 재발급받는 과정을 UseCase 메서드 하나로 둔 이유는, 비즈니스 로직에서 담당하는 게 맞다고 생각했기 때문입니다.
+  ///   물론 이를 두 개의 메서드로 나누어 ViewModel에서 연결해주어도 상관 없습니다.
   public func reissueToken() -> AnyPublisher<Bool, Never> {
     return repository.reissueRefreshToken()
       .map(\.refreshToken)
