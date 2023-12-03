05db57086c62c79069fcb2afce63abc539c4b7b3
SeungHyun Hong <whi7ehyun@gmail.com>
2023-12-03 16:32:18 +0900

    add: Logo image 추가

---
+++ iOS/Projects/Shared/DesignSystem/Sources/UIImage+assets.swift
@@ -0,0 +1,13 @@
+//
+//  UIImage+assets.swift
+//  DesignSystem
+//
+//  Created by 홍승현 on 12/3/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import UIKit
+
+public extension UIImage {
+  static let logoImage: UIImage = .logo
+}
