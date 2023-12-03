78a2355f3b691ce41d23b95779f2a84aa3cbbb9e
JongPyoAhn <whdvy95@gmail.com>
2023-12-04 00:10:12 +0900

    docs: Cacher 모듈에 주석 추가

--- iOS/Projects/Core/Cacher/Sources/Cacher.swift
+++ iOS/Projects/Core/Cacher/Sources/Cacher.swift
@@ -24,7 +24,9 @@ final class Cacher {
   init(fileManager: FileManager) {
     self.fileManager = fileManager
   }
-
+  /// memory와 disk에서 데이터를 가져옵니다.
+  /// memory캐시를 진행하고 메모리에 데이터가 없으면 디스크캐시로 넘어갑니다. 그마저 없다면 Error를 방출합니다.
+  /// Error는 나중에 API호출로 처리됩니다.
   func fetch(cacheKey: String) throws -> Data? {
     if let memoryData = fetchMemoryData(cacheKey: cacheKey) {
       return memoryData
@@ -36,15 +38,18 @@ final class Cacher {
     throw CacheError.invalidData
   }
 
+  /// memory와 disk에 캐시할 데이터를 저장합니다.
   func set(data: Data, cacheKey: String) throws {
     setMemory(data: data, cacheKey: cacheKey)
     try setDisk(data: data, cacheKey: cacheKey)
   }
-
+  
+  /// memory데이터를 불러옵니다.
   func fetchMemoryData(cacheKey: String) -> Data? {
     return cache.object(forKey: cacheKey as NSString) as? Data
   }
 
+  /// disk데이터를 불러옵니다.
   func fetchDiskData(cacheKey: String) throws -> Data? {
     guard let directoryURL = generateFileURL(cacheKey: cacheKey) else {
       throw CacheError.invalidFileURL
@@ -54,10 +59,12 @@ final class Cacher {
     return data
   }
 
+  /// memory에 데이터를 저장합니다.
   func setMemory(data: Data, cacheKey: String) {
     cache.setObject(data as NSData, forKey: cacheKey as NSString)
   }
 
+  /// disk에 데이터를 저장합니다.
   func setDisk(data: Data, cacheKey: String) throws {
     guard let url = generateFileURL(cacheKey: cacheKey) else {
       throw CacheError.invalidFileURL
@@ -71,6 +78,7 @@ final class Cacher {
     fileManager.createFile(atPath: appendedPath, contents: data)
   }
 
+  /// cache 디렉토리가 존재하는 URL을 구합니다.
   private func generateFileURL(cacheKey: String) -> URL? {
     return fileManager.urls(
       for: .cachesDirectory,
