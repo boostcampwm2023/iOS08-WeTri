addd90565ef4f9e99d45aa51128decc1d8eecfd4
SeungHyun Hong <whi7ehyun@gmail.com>
2023-12-04 16:26:47 +0900

    refactor: ProfileHeaderView를 두고 UICollectionViewController로 전환
    게시글이 많이 존재할 때 상단에 있는 사용자 프로필과 닉네임도 같이 스크롤되어 올라가야합니다.
    이를 구현하고자 ReusableView로 변경했습니다.

---
+++ iOS/Projects/Features/Profile/Sources/ProfileScene/ProfileHeaderView.swift
@@ -0,0 +1,108 @@
+//
+//  ProfileHeaderView.swift
+//  ProfileFeature
+//
+//  Created by 홍승현 on 12/4/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import DesignSystem
+import UIKit
+
+// MARK: - ProfileHeaderView
+
+final class ProfileHeaderView: UICollectionReusableView {
+  // MARK: UI Components
+
+  private let profileImageView: UIImageView = {
+    let imageView = UIImageView(image: .init(systemName: "person.fill"))
+    imageView.contentMode = .scaleAspectFill
+    imageView.tintColor = DesignSystemColor.main03 // 기본 이미지에 메인 색상을 적용하기 위함
+    imageView.backgroundColor = DesignSystemColor.gray01
+    imageView.layer.borderWidth = 2
+    imageView.layer.cornerRadius = Metrics.profileSize * 0.5
+    imageView.layer.borderColor = DesignSystemColor.main03.cgColor
+    imageView.clipsToBounds = true
+    return imageView
+  }()
+
+  private let nicknameLabel: UILabel = {
+    let label = UILabel()
+    label.text = "..."
+    label.font = .preferredFont(forTextStyle: .title1, weight: .bold)
+    label.textColor = DesignSystemColor.primaryText
+    return label
+  }()
+
+  private let profileStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.alignment = .center
+    stackView.spacing = 12
+    return stackView
+  }()
+
+  private let postSectionLabel: UILabel = {
+    let label = UILabel()
+    label.text = "내 게시물"
+    label.textColor = DesignSystemColor.primaryText
+    label.font = .preferredFont(forTextStyle: .title2, weight: .medium)
+    return label
+  }()
+
+  // MARK: Initializations
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupLayouts()
+    setupConstraints()
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    setupLayouts()
+    setupConstraints()
+  }
+
+  // MARK: - Configurations
+
+  private func setupLayouts() {
+    addSubview(profileStackView)
+    addSubview(postSectionLabel)
+    for view in [profileImageView, nicknameLabel] {
+      profileStackView.addArrangedSubview(view)
+    }
+  }
+
+  private func setupConstraints() {
+    profileStackView.translatesAutoresizingMaskIntoConstraints = false
+    profileImageView.translatesAutoresizingMaskIntoConstraints = false
+    postSectionLabel.translatesAutoresizingMaskIntoConstraints = false
+
+    NSLayoutConstraint.activate(
+      [
+        profileStackView.topAnchor.constraint(equalTo: topAnchor, constant: Metrics.profileStackViewTop),
+        profileStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Metrics.horizontal),
+
+        profileImageView.widthAnchor.constraint(equalToConstant: Metrics.profileSize),
+        profileImageView.heightAnchor.constraint(equalToConstant: Metrics.profileSize),
+
+        postSectionLabel.topAnchor.constraint(equalTo: profileStackView.bottomAnchor, constant: Metrics.postSectionLabelTop),
+        postSectionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Metrics.horizontal),
+        postSectionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Metrics.postSectionLabelBottom),
+      ]
+    )
+  }
+}
+
+// MARK: ProfileHeaderView.Metrics
+
+private extension ProfileHeaderView {
+  enum Metrics {
+    static let profileSize: CGFloat = 60
+    static let profileStackViewTop: CGFloat = 18
+    static let horizontal: CGFloat = 24
+
+    static let postSectionLabelTop: CGFloat = 30
+    static let postSectionLabelBottom: CGFloat = 18
+  }
+}
--- iOS/Projects/Features/Profile/Sources/ProfileScene/ProfileViewController.swift
+++ iOS/Projects/Features/Profile/Sources/ProfileScene/ProfileViewController.swift
@@ -5,53 +5,20 @@ import UIKit
 
 // MARK: - ProfileViewController
 
-public final class ProfileViewController: UIViewController {
+public final class ProfileViewController: UICollectionViewController {
   // MARK: Properties
+
+  private var dataSource: ProfileDataSource?
 
   private let viewModel: ProfileViewModelRepresentable
 
   // MARK: UI Components
 
-  private let profileImageView: UIImageView = {
-    let imageView = UIImageView(image: .init(systemName: "person.fill"))
-    imageView.contentMode = .scaleAspectFill
-    imageView.tintColor = DesignSystemColor.main03 // 기본 이미지에 메인 색상을 적용하기 위함
-    imageView.backgroundColor = DesignSystemColor.gray01
-    imageView.layer.borderWidth = 2
-    imageView.layer.cornerRadius = Metrics.profileSize * 0.5
-    imageView.layer.borderColor = DesignSystemColor.main03.cgColor
-    imageView.clipsToBounds = true
-    return imageView
-  }()
-
-  private let nicknameLabel: UILabel = {
-    let label = UILabel()
-    label.text = "..."
-    label.font = .preferredFont(forTextStyle: .title1, weight: .bold)
-    label.textColor = DesignSystemColor.primaryText
-    return label
-  }()
-
-  private let profileStackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.alignment = .center
-    stackView.spacing = 12
-    return stackView
-  }()
-
-  private let postSectionLabel: UILabel = {
-    let label = UILabel()
-    label.text = "내 게시물"
-    label.textColor = DesignSystemColor.primaryText
-    label.font = .preferredFont(forTextStyle: .title2, weight: .medium)
-    return label
-  }()
-
   // MARK: Initializations
 
   public init(viewModel: ProfileViewModelRepresentable) {
     self.viewModel = viewModel
-    super.init(nibName: nil, bundle: nil)
+    super.init(collectionViewLayout: Self.createProfileLayout())
   }
 
   @available(*, unavailable)
@@ -63,37 +30,7 @@ public final class ProfileViewController: UIViewController {
 
   override public func viewDidLoad() {
     super.viewDidLoad()
-    setupLayouts()
-    setupConstraints()
     setupStyles()
-  }
-
-  private func setupLayouts() {
-    view.addSubview(profileStackView)
-    view.addSubview(postSectionLabel)
-    for view in [profileImageView, nicknameLabel] {
-      profileStackView.addArrangedSubview(view)
-    }
-  }
-
-  private func setupConstraints() {
-    let safeArea = view.safeAreaLayoutGuide
-    profileStackView.translatesAutoresizingMaskIntoConstraints = false
-    profileImageView.translatesAutoresizingMaskIntoConstraints = false
-    postSectionLabel.translatesAutoresizingMaskIntoConstraints = false
-
-    NSLayoutConstraint.activate(
-      [
-        profileStackView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: Metrics.profileStackViewTop),
-        profileStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: Metrics.horizontal),
-
-        profileImageView.widthAnchor.constraint(equalToConstant: Metrics.profileSize),
-        profileImageView.heightAnchor.constraint(equalToConstant: Metrics.profileSize),
-
-        postSectionLabel.topAnchor.constraint(equalTo: profileStackView.bottomAnchor, constant: Metrics.postSectionLabelTop),
-        postSectionLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: Metrics.horizontal),
-      ]
-    )
   }
 
   private func setupStyles() {
@@ -113,14 +50,70 @@ public final class ProfileViewController: UIViewController {
   }
 }
 
-// MARK: ProfileViewController.Metrics
+// MARK: - Compositional Layout Settings
 
 private extension ProfileViewController {
-  enum Metrics {
-    static let profileSize: CGFloat = 60
-    static let profileStackViewTop: CGFloat = 18
-    static let horizontal: CGFloat = 24
-
-    static let postSectionLabelTop: CGFloat = 30
+  private static func createProfileLayout() -> UICollectionViewLayout {
+    return UICollectionViewCompositionalLayout { sectionNumber, _ in
+      if sectionNumber == Section.header.rawValue {
+        return createHeaderSection()
+      } else {
+        return createMainSection()
+      }
+    }
   }
+
+  private static func createHeaderSection() -> NSCollectionLayoutSection {
+    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(1)) // 높이를 1으로 설정, 아무 값도 넣지 않을 예정임
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+    let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(1)) // 높이를 1으로 설정, 아무 값도 넣지 않을 예정임
+    let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+    let section = NSCollectionLayoutSection(group: group)
+
+    let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(100)) // 대략 100으로 설정함
+    let header = NSCollectionLayoutBoundarySupplementaryItem(
+      layoutSize: headerSize,
+      elementKind: UICollectionView.elementKindSectionHeader,
+      alignment: .top
+    )
+    section.boundarySupplementaryItems = [header]
+
+    return section
+  }
+
+  private static func createMainSection() -> NSCollectionLayoutSection {
+    // 각 아이템의 사이즈를 정의합니다.
+    let itemSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalWidth(1 / 3),
+      heightDimension: .fractionalHeight(1)
+    )
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+    item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+
+    let groupSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalWidth(1),
+      heightDimension: .fractionalWidth(1 / 3)
+    )
+
+    let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+    // 섹션 정의
+    return NSCollectionLayoutSection(group: group)
+  }
+}
+
+private extension ProfileViewController {
+  typealias ProfileDataSource = UICollectionViewDiffableDataSource<Section, Item>
+  typealias ProfileSnapshot = NSDiffableDataSourceSnapshot<Section, Item>
+  typealias ProfileCellRegistration = UICollectionView.CellRegistration<UICollectionViewCell, Item>
+  typealias ProfileReusableRegistration = UICollectionView.SupplementaryRegistration<ProfileHeaderView>
+
+  enum Section: Int {
+    case header
+    case main
+  }
+
+  typealias Item = String
 }
