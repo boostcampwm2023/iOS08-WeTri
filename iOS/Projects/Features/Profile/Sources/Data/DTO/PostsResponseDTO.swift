9d3377dea97d689ed3adda11c4caf736dea6407c
SeungHyun Hong <whi7ehyun@gmail.com>
2023-12-04 23:42:13 +0900

    add: PostsResponstDTO, 그에 맞는 mock json 추가

---
+++ iOS/Projects/Features/Profile/Sources/Data/DTO/PostsResponseDTO.swift
@@ -0,0 +1,58 @@
+//
+//  PostsResponseDTO.swift
+//  ProfileFeature
+//
+//  Created by 홍승현 on 12/4/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import Foundation
+
+struct PostsResponseDTO: Codable {
+
+  /// 게시글 목록
+  let posts: [Post]
+
+  /// 받아온 게시글의 메타데이터
+  let metaData: MetaData
+
+  enum CodingKeys: String, CodingKey {
+    case posts = "items"
+    case metaData
+  }
+}
+
+// MARK: - Item
+
+/// 게시글
+struct Post: Codable {
+  /// 게시글 Identifier
+  let id: Int
+
+  /// 게시글 내용
+  let content: String
+
+  /// 게시글 이미지 URL
+  let postURL: URL
+
+  enum CodingKeys: String, CodingKey {
+    case id
+    case content
+    case postURL = "postUrl"
+  }
+}
+
+// MARK: - MetaData
+
+struct MetaData: Codable {
+  /// 다음에 요청으로 보낼 id값
+  let nextID: Int
+
+  /// 받아온 데이터 개수
+  let count: Int
+
+  enum CodingKeys: String, CodingKey {
+    case nextID = "after"
+    case count
+  }
+}
