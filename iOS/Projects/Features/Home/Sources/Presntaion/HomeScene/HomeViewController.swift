0cc5e2dc9cb5f13a2866ef3fb6df9a063b9ead6b
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2023-12-05 18:54:00 +0900

    feat: Home Preoject.swift 생성

---
+++ iOS/Projects/Features/Home/Sources/Presntaion/HomeScene/HomeViewController.swift
@@ -0,0 +1,77 @@
+// 
+//  HomeViewController.swift
+//  HomeFeature
+//
+//  Created by MaraMincho on 12/5/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import Combine
+import DesignSystem
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+  // MARK: Properties
+
+  private let viewModel: HomeViewModelRepresentable
+
+  private var subscriptions: Set<AnyCancellable> = []
+
+  // MARK: UI Components
+
+  private let button: UIButton = .init(configuration: .mainEnabled(title: "test button"))
+
+  // MARK: Initializations
+
+  init(viewModel: HomeViewModelRepresentable) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  @available(*, unavailable)
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: Life Cycles
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setup()
+  }
+
+
+}
+
+private extension HomeViewController {
+  func setup() {
+    setupStyles()
+    setupHierarchyAndConstraints()
+    bind()
+  }
+  
+  func setupHierarchyAndConstraints() {
+    let safeArea = view.safeAreaLayoutGuide
+    
+  }
+  
+  func setupStyles() {
+    view.backgroundColor = DesignSystemColor.primaryBackground
+  }
+  
+  func bind() {
+    let output = viewModel.transform(input: .init())
+    output.sink { state in
+      switch state {
+      case .idle:
+        break
+      }
+    }
+    .store(in: &subscriptions)
+  }
+  
+  enum Metrics {
+    
+  }
+}
---
+++ iOS/Projects/Features/Home/Sources/Presntaion/HomeScene/HomeViewModel.swift
@@ -0,0 +1,46 @@
+// 
+//  HomeViewModel.swift
+//  HomeFeature
+//
+//  Created by MaraMincho on 12/5/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+// MARK: - HomeViewModelInput
+
+public struct HomeViewModelInput {}
+
+public typealias HomeViewModelOutput = AnyPublisher<HomeState, Never>
+
+// MARK: - HomeState
+
+public enum HomeState {
+  case idle
+}
+
+// MARK: - HomeViewModelRepresentable
+
+protocol HomeViewModelRepresentable {
+  func transform(input: HomeViewModelInput) -> HomeViewModelOutput
+}
+
+final class HomeViewModel {
+
+  // MARK: - Properties
+
+  private var subscriptions: Set<AnyCancellable> = []
+}
+
+extension HomeViewModel: HomeViewModelRepresentable {
+  public func transform(input: HomeViewModelInput) -> HomeViewModelOutput {
+    subscriptions.removeAll()
+
+
+    let initialState: HomeViewModelOutput = Just(.idle).eraseToAnyPublisher()
+
+    return initialState
+  }
+}
