e5476e11963614b331206d4308b6fa2f4a44e00e
wonholim <kds0034@gmail.com>
2023-12-06 16:57:49 +0900

    feat: 이미지 DTO 작성 및 스웨거 작성

--- BackEnd/src/images/dto/images.response.ts
+++ BackEnd/src/images/dto/images.response.ts
@@ -1,14 +1,43 @@
-export const ImagesResponse = () => {
-  return {
-    example: {
-      code: null,
-      errorMessage: null,
-      data: [
-        {
-          imageName: '스크린샷 2023-04-09 오후 6.46.20.png',
-          imageUrl: 'https://cdnAdress/uuid.png',
-        },
-      ],
+import {ApiProperty} from "@nestjs/swagger";
+import {SuccessResDto} from "../../common/dto/SuccessRes.dto";
+import {IsNotEmpty, IsString} from "class-validator";
+
+export class ImageRequestDto {
+  @ApiProperty({
+    type: 'array',
+    items: {
+      type: 'string',
+      format: 'binary',
+      description: 'File upload',
     },
-  };
-};
+    description: '이미지 파일, 10MB 이하 .png .jpg .jpeg 파일만 가능',
+  })
+  images: any[];
+}
+
+export class ImageResponse {
+  @ApiProperty({
+    description: '이미지 이름',
+    example: '스크린샷 2023-04-09 오후 6.46.20.png',
+  })
+  @IsString()
+  @IsNotEmpty()
+  imageName: string;
+  @ApiProperty({
+    description: '저장된 이미지 URL',
+    example: 'https://cdnAdress/uuid.png',
+  })
+  @IsString()
+  @IsNotEmpty()
+  imageUrl: string;
+
+  constructor(imageName: string, imageUrl: string) {
+    this.imageName = imageName;
+    this.imageUrl = imageUrl;
+  }
+}
+
+export class ImageResponseDto extends SuccessResDto{
+  @ApiProperty({ type: () => [ImageResponse] })
+  data: ImageResponse[];
+}
