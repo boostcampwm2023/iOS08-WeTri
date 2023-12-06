9dffa20c2411d81f4a6f1e0b09fe3b12baa20bb2
SeungHyun Hong <whi7ehyun@gmail.com>
2023-12-06 23:05:39 +0900

    add: UserProfileHeaderView 추가

---
+++ iOS/Projects/Features/Profile/Sources/Presentation/ProfileSettingsScene/UserProfileHeaderView.swift
@@ -0,0 +1,101 @@
+//
+//  UserProfileHeaderView.swift
+//  ProfileFeature
+//
+//  Created by 홍승현 on 12/6/23.
+//  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
+//
+
+import DesignSystem
+import UIKit
+
+// MARK: - UserProfileHeaderView
+
+final class UserProfileHeaderView: UICollectionReusableView {
+  // MARK: UI Components
+
+  private let imageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFill
+    imageView.clipsToBounds = true
+    imageView.layer.cornerRadius = Metrics.imageViewSize * 0.5
+    return imageView
+  }()
+
+  private let nicknameLabel: UILabel = {
+    let label = UILabel()
+    label.text = "NickName"
+    label.font = .preferredFont(forTextStyle: .callout)
+    label.textColor = DesignSystemColor.primaryText
+    return label
+  }()
+
+  private let birthLabel: UILabel = {
+    let label = UILabel()
+    label.text = "1900. 00. 00"
+    label.font = .preferredFont(forTextStyle: .callout)
+    label.textColor = DesignSystemColor.gray03
+    return label
+  }()
+
+  // MARK: Initializations
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupLayouts()
+    setupConstraints()
+    setupStyles()
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    setupLayouts()
+    setupConstraints()
+    setupStyles()
+  }
+
+  // MARK: Configurations
+
+  private func setupLayouts() {
+    addSubview(imageView)
+    addSubview(nicknameLabel)
+    addSubview(birthLabel)
+  }
+
+  private func setupConstraints() {
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    nicknameLabel.translatesAutoresizingMaskIntoConstraints = false
+    birthLabel.translatesAutoresizingMaskIntoConstraints = false
+
+    NSLayoutConstraint.activate(
+      [
+        imageView.topAnchor.constraint(equalTo: topAnchor, constant: Metrics.imageViewTop),
+        imageView.widthAnchor.constraint(equalToConstant: Metrics.imageViewSize),
+        imageView.heightAnchor.constraint(equalToConstant: Metrics.imageViewSize),
+        imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+
+        nicknameLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+        nicknameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: Metrics.nicknameLabelTop),
+
+        birthLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+        birthLabel.topAnchor.constraint(equalTo: nicknameLabel.bottomAnchor, constant: Metrics.birthLabelTop),
+      ]
+    )
+  }
+
+  private func setupStyles() {
+    backgroundColor = DesignSystemColor.primaryBackground
+  }
+}
+
+// MARK: UserProfileHeaderView.Metrics
+
+private extension UserProfileHeaderView {
+  enum Metrics {
+    static let imageViewSize: CGFloat = 80
+
+    static let imageViewTop: CGFloat = 24
+    static let nicknameLabelTop: CGFloat = 16
+    static let birthLabelTop: CGFloat = 8
+  }
+}
