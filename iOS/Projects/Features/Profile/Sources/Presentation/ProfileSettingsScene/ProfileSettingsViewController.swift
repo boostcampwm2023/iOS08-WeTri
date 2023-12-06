bf5b1d604b5e8146081383090cf80e8e1abff1ba
SeungHyun Hong <whi7ehyun@gmail.com>
2023-12-06 23:18:56 +0900

    add: ProfileSettingsDataSource 로직 추가

--- iOS/Projects/Features/Profile/Sources/Presentation/ProfileSettingsScene/ProfileSettingsViewController.swift
+++ iOS/Projects/Features/Profile/Sources/Presentation/ProfileSettingsScene/ProfileSettingsViewController.swift
@@ -10,12 +10,16 @@ import Combine
 import DesignSystem
 import UIKit
 
+// MARK: - ProfileSettingsViewController
+
 final class ProfileSettingsViewController: UICollectionViewController {
   // MARK: Properties
 
   private let viewModel: ProfileSettingsViewModelRepresentable
 
   private var subscriptions: Set<AnyCancellable> = []
+
+  private var dataSource: ProfileSettingsDataSource?
 
   // MARK: Initializations
 
@@ -52,5 +56,41 @@ final class ProfileSettingsViewController: UICollectionViewController {
       }
     }
     .store(in: &subscriptions)
+  }
+}
+
+private extension ProfileSettingsViewController {
+  typealias ProfileSettingsDataSource = UICollectionViewDiffableDataSource<Section, Item>
+  typealias ProfileSettingsSnapshot = NSDiffableDataSourceSnapshot<Section, Item>
+  typealias ListCellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Item>
+  enum Section {
+    case main
+  }
+
+  enum Item: String, CaseIterable {
+    case nicknameSetting = "닉네임 설정"
+    case photoSetting = "프로필 사진 설정"
+  }
+
+  private func setupDataSource() {
+    let registration = ListCellRegistration { cell, _, itemIdentifier in
+      var configuration = cell.defaultContentConfiguration()
+      configuration.text = itemIdentifier.rawValue
+      cell.contentConfiguration = configuration
+    }
+
+    let dataSource = ProfileSettingsDataSource(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
+      collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: itemIdentifier)
+    }
+
+    self.dataSource = dataSource
+  }
+
+  private func setupInitialSnapshots() {
+    guard let dataSource else { return }
+    var snapshot = ProfileSettingsSnapshot()
+    snapshot.appendSections([.main])
+    snapshot.appendItems(Item.allCases, toSection: .main)
+    dataSource.apply(snapshot, animatingDifferences: false)
   }
 }
