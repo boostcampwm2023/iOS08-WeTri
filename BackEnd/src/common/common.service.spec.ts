edfc586427977d42cdb3fc61de291aa829ec7a79
jeong-yong-shin <jeong-yong-shin@Sjy-MacBookProM1.local>
2023-12-07 21:35:36 +0900

    test: common.service.spec.ts 작성

--- BackEnd/src/common/common.service.spec.ts
+++ BackEnd/src/common/common.service.spec.ts
@@ -1,29 +1,46 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { CommonService } from "./common.service"
-import { BaseEntity, QueryBuilder, SelectQueryBuilder } from "typeorm";
+import { QueryFailedError, Repository } from "typeorm";
+import { posts } from "../posts/mocks/mocks";
+import { Post } from "../posts/entities/posts.entity";
+import { mockGetCreateUpdateQueryOptions, mockItems, mockPaginationDto } from "./mocks/mocks";
+import { InternalServerErrorException } from "@nestjs/common";
+import { getRepositoryToken } from "@nestjs/typeorm";
 
 describe('commonService', () => {
     let service: CommonService;
-
+    let postsRepository: Repository<Post>;
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [
                 CommonService,
+                {
+                    provide: getRepositoryToken(Post),
+                    useValue: {
+                        createQueryBuilder: jest.fn()
+                    }
+                }
             ]
         }).compile();
-        
         service = module.get<CommonService>(CommonService);
+        postsRepository = module.get<Repository<Post>>(getRepositoryToken(Post));
     })
 
     describe('paginate', () => {
         const mockQueryBuilder = {
-            setFindOptions: jest.fn().mockReturnThis(),
-            leftJoin: jest.fn().mockReturnThis(),
-            select: jest.fn().mockReturnThis(),
-            getMany: jest.fn().mockResolvedValue([]),
-          } as any
-        it('QueryBuilder가 잘못 설정되어 있다면 InternalServerErrorException 에러 ', async () => {
-            jest.spyOn(service,'makeQueryBuilder').mockReturnValue(mockQueryBuilder)
+            getMany: jest.fn()
+        } as any
+        it('QueryBuilder가 잘못 설정되어 있다면 InternalServerErrorException 에러', async () => {
+            jest.spyOn(service, 'makeQueryBuilder').mockReturnValue(mockQueryBuilder);
+            jest.spyOn(mockQueryBuilder, 'getMany').mockRejectedValue(new QueryFailedError('SELECT *', [], 'Error message'));
+            await expect(service.paginate(mockPaginationDto, postsRepository, mockGetCreateUpdateQueryOptions)).rejects.toThrow(InternalServerErrorException);
+        })
+
+        it('게시글 요청하면 paginate 함수를 실행해서 요청한 게시글들, metaData 반환', async () => {
+            jest.spyOn(service, 'makeQueryBuilder').mockReturnValue(mockQueryBuilder);
+            jest.spyOn(mockQueryBuilder, 'getMany').mockResolvedValue(mockItems);
+            const result = await service.paginate(mockPaginationDto, postsRepository, mockGetCreateUpdateQueryOptions);
+            expect(result).toEqual(posts);
         })
     })
 })
