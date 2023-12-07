513d7f5a0e5a0fd9236b2d65de076a3db91d4336
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2023-12-07 11:09:49 +0900

    feat: 프로필 피드 카드 셀 상단부 구현

--- iOS/Projects/Features/Home/Sources/Presntaion/HomeScene/VIew/FeedItemCollectionViewCell.swift
+++ iOS/Projects/Features/Home/Sources/Presntaion/HomeScene/VIew/FeedItemCollectionViewCell.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
 //
 
+import DesignSystem
 import UIKit
 
 // MARK: - FeedItemCollectionViewCell
@@ -22,14 +23,97 @@ class FeedItemCollectionViewCell: UICollectionViewCell {
   required init?(coder _: NSCoder) {
     fatalError("생성할 수 없습니다.")
   }
+
+  //MARK: - Property
+  //MARK: - 프로필 이미지와 정보들을 알 수 있는 Property들의 모음입니다.
+  
+  private let profileImage: UIImageView = {
+    let imageView = UIImageView()
+
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+
+  private let nickNameLabel: UILabel = {
+    let label = UILabel()
+    label.text = "위트라이"
+    label.font = .preferredFont(forTextStyle: .title1)
+    label.textColor = DesignSystemColor.primaryText
+
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  private let sportLabel: UILabel = {
+    let label = UILabel()
+    label.text = "달리기"
+    label.font = .preferredFont(forTextStyle: .title3)
+    label.textColor = DesignSystemColor.primaryText
+
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  private let dateLabel: UILabel = {
+    let label = UILabel()
+    label.text = "2023.12.07"
+    label.font = .preferredFont(forTextStyle: .title3)
+    label.textColor = DesignSystemColor.primaryText
+
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  lazy var sportLabelAndNickdateLabelStackView: UIStackView = {
+    let stackView = UIStackView(arrangedSubviews: [
+      sportLabel,
+      dateLabel,
+    ])
+    stackView.spacing = Metrics.sportLabelAndNickdateLabelSpacing
+    stackView.axis = .horizontal
+
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    return stackView
+  }()
+  
+  lazy var nickNameLabelAndSportDateLabelStackView: UIStackView = {
+    let stackView = UIStackView(arrangedSubviews: [
+      nickNameLabel,
+      sportLabelAndNickdateLabelStackView
+    ])
+    stackView.axis = .vertical
+    stackView.spacing = 4
+    
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    return stackView
+  }()
+  
+  lazy var cardHeaderStackView: UIStackView = {
+    let stackView = UIStackView(arrangedSubviews: [
+      profileImage,
+      nickNameLabelAndSportDateLabelStackView
+    ])
+    stackView.axis = .horizontal
+    stackView.spacing = 12
+    
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    return stackView
+  }()
+  
+  //MARK: - 본문 내용에 관한 Property입니다.
+  
 }
 
-extension FeedItemCollectionViewCell {
+private extension FeedItemCollectionViewCell {
   func setup() {
     setStyle()
   }
 
   func setStyle() {
     backgroundColor = .red
+  }
+  
+  enum Metrics {
+    static let sportLabelAndNickdateLabelSpacing: CGFloat = 3
   }
 }
