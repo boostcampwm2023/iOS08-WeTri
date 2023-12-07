4914c77615026b81083702a4aeffef3a897b148a
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2023-12-07 12:02:00 +0900

    feat: headerView 실제 테스트

--- iOS/Projects/Features/Home/Sources/Presntaion/HomeScene/VIew/FeedItemCollectionViewCell.swift
+++ iOS/Projects/Features/Home/Sources/Presntaion/HomeScene/VIew/FeedItemCollectionViewCell.swift
@@ -30,6 +30,7 @@ class FeedItemCollectionViewCell: UICollectionViewCell {
 
   private let profileImage: UIImageView = {
     let imageView = UIImageView()
+    imageView.backgroundColor = .blue
 
     imageView.translatesAutoresizingMaskIntoConstraints = false
     return imageView
@@ -102,11 +103,66 @@ class FeedItemCollectionViewCell: UICollectionViewCell {
   }()
 
   // MARK: - 본문 내용에 관한 Property입니다.
+
+  private let feedDetailTextLabel: UILabel = {
+    let label = UILabel()
+    label.text = Constants.feedDetailText
+    label.font = .preferredFont(forTextStyle: .body)
+    label.numberOfLines = 2
+    label.textColor = DesignSystemColor.primaryText
+
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  private let feedDetailImageView: UIImageView = {
+    let imageView = UIImageView()
+
+    imageView.backgroundColor = .blue
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+
+  private lazy var cardBodyStackView: UIStackView = {
+    let stackView = UIStackView(arrangedSubviews: [
+      feedDetailTextLabel,
+      feedDetailImageView,
+    ])
+    stackView.spacing = 16
+    stackView.axis = .vertical
+
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    return stackView
+  }()
 }
 
 private extension FeedItemCollectionViewCell {
+  func setupViewHierarchyAndConstraints() {
+    let safeArea = safeAreaLayoutGuide
+
+    contentView.addSubview(cardHeaderStackView)
+    cardHeaderStackView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+    cardHeaderStackView.leadingAnchor
+      .constraint(equalTo: safeArea.leadingAnchor, constant: ConstraintsGuideLine.value).isActive = true
+    cardHeaderStackView.trailingAnchor
+      .constraint(equalTo: safeArea.trailingAnchor, constant: -ConstraintsGuideLine.value).isActive = true
+
+//    contentView.addSubview(cardBodyStackView)
+//    cardBodyStackView.topAnchor
+//      .constraint(equalTo: cardHeaderStackView.bottomAnchor, constant: 16).isActive = true
+//    cardBodyStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: <#T##CGFloat#>)
+
+    // 이미지 뷰 너비와 높이 조정
+    profileImage.widthAnchor.constraint(equalToConstant: 56).isActive = true
+    profileImage.heightAnchor.constraint(equalToConstant: 56).isActive = true
+
+    feedDetailImageView.widthAnchor.constraint(equalToConstant: 291).isActive = true
+    feedDetailImageView.heightAnchor.constraint(equalToConstant: 246).isActive = true
+  }
+
   func setup() {
     setStyle()
+    setupViewHierarchyAndConstraints()
   }
 
   func setStyle() {
@@ -117,5 +173,9 @@ private extension FeedItemCollectionViewCell {
     static let sportLabelAndNickdateLabelSpacing: CGFloat = 3
     static let nickNameLabelAndSportDateLabelSpacing: CGFloat = 4
     static let profileImageAndUserInformationLabelStackViewSpacing: CGFloat = 12
+  }
+
+  enum Constants {
+    static let feedDetailText = "정당은 그 목적·조직과 활동이 민주적이어야 하며, 국민의 정치적 의사형성에 참여하는데 필요한 조직을 가져야 한다. 대통령은 내우·외환·천재·지변 또는 중대한 재정·경제상의 위기에 있어서 없을 때에 한하여 최소한으로 필"
   }
 }
