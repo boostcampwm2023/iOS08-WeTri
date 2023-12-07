53d9fd0cf9f853f30a7fad9017f455b02ae397bc
SeungHyun Hong <whi7ehyun@gmail.com>
2023-12-07 21:05:40 +0900

    chore: LocationPathUseCase 로직 수정

--- iOS/Projects/Features/Record/Sources/Domain/UseCases/LocationPathUseCase.swift
+++ iOS/Projects/Features/Record/Sources/Domain/UseCases/LocationPathUseCase.swift
@@ -10,27 +10,12 @@ import Foundation
 
 struct LocationPathUseCase: LocationPathUseCaseRepresentable {
   func processPath(locations: [LocationDTO]) -> MapRegion {
-    guard let firstLocation = locations.first else {
-      return MapRegion(minLatitude: 0, maxLatitude: 0, minLongitude: 0, maxLongitude: 0)
-    }
-
-    var minLatitude = firstLocation.latitude
-    var minLongitude = firstLocation.longitude
-    var maxLatitude = minLatitude
-    var maxLongitude = minLongitude
-
-    for location in locations {
-      minLatitude = min(minLatitude, location.latitude)
-      minLongitude = min(minLongitude, location.longitude)
-      maxLatitude = max(maxLatitude, location.latitude)
-      maxLongitude = max(maxLongitude, location.longitude)
-    }
 
     return MapRegion(
-      minLatitude: minLatitude,
-      maxLatitude: maxLatitude,
-      minLongitude: minLongitude,
-      maxLongitude: maxLongitude
+      minLatitude: locations.map(\.latitude).min() ?? 0,
+      maxLatitude: locations.map(\.latitude).max() ?? 0,
+      minLongitude: locations.map(\.longitude).min() ?? 0,
+      maxLongitude: locations.map(\.longitude).max() ?? 0
     )
   }
 }
