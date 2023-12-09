2b6a92ee650f4d98c81ab8d6823dddebd2e0ee98
wonholim <kds0034@gmail.com>
2023-12-09 20:00:42 +0900

    fix: record 스웨거 수정
    Co-authored-by: sjy982 <sjy982@gmail.com>

--- BackEnd/src/records/dto/record-response.dto.ts
+++ BackEnd/src/records/dto/record-response.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { SuccessResDto } from '../../common/dto/SuccessRes.dto';
 import { Record } from '../entities/records.entity';
-
+import { Workout } from 'src/workouts/entities/workout.entity';
+class WorkoutDto extends PickType(Workout, ['name']) {}
 class GetRecord extends PickType(Record, [
-  'workout',
   'profile',
   'workoutTime',
   'distance',
@@ -14,20 +14,19 @@ class GetRecord extends PickType(Record, [
   'createdAt',
   'mapCapture',
   'gps'
-]) {}
-
+]) {
+  @ApiProperty( {type: () => WorkoutDto} )
+  workout: WorkoutDto;
+}
 class GetRecordWithId extends PickType(Record, ['id']) {}
-
 export class CreateRecordResDto extends SuccessResDto {
   @ApiProperty({ type: () => GetRecordWithId })
   data: Pick<GetRecordWithId, 'id'>;
 }
-
 export class GetUsersRecordsResDto extends SuccessResDto {
   @ApiProperty({ type: () => [GetRecord] })
   data: GetRecord[];
 }
-
 export class GetRecordResDto extends SuccessResDto {
   @ApiProperty({ type: () => GetRecord })
   data: GetRecord;
