1f1ce7b5b21c8d0a5210b8b30ad5f9120b70144a
MaraMincho <seemaster300@naver.com>
2024-01-09 14:57:23 +0900

    feat: 칼만필터 발열 이슈 수정

--- iOS/Projects/Features/Record/Sources/Domain/UseCases/KalmanFilter.swift
+++ iOS/Projects/Features/Record/Sources/Domain/UseCases/KalmanFilter.swift
@@ -6,29 +6,26 @@
 //  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
 //
 
+import CoreLocation
 import Foundation
 import Log
+import simd
 
 struct KalmanFilter {
   /// 새로운 값을 입력하게 된다면, 에측을 통해서 값을 작성하게 되는 변수 입니다.
-  var x = MatrixOfTwoDimension([[]])
+  var x = simd_double4()
 
-  /// 초기 오차 공분산 입니다.
-  /// 초기 값은 에러가 많기 떄문에 다음과 같이 크게 가져갔습니다.
-  private var p = MatrixOfTwoDimension([
-    [500, 0, 0, 0],
+  private var p = simd_double4x4([
+    [1, 0, 0, 0],
     [0, 1, 0, 0],
-    [0, 0, 500, 0],
+    [0, 0, 1, 0],
     [0, 0, 0, 1],
   ])
 
-  // 사용자 경험을 통해 얻어진 값 입니다. (일단 대한민국 GPS환경이 좋다고 가정하여,
-  // 애플 오차의 1/2로 가져갔습니다.)
-
-  private var q = MatrixOfTwoDimension([
-    [0.000455, 0, 0, 0],
+  private var q = simd_double4x4([
+    [0.00082, 0, 0, 0],
     [0, 0, 0, 0],
-    [0, 0, 0.000059, 0],
+    [0, 0, 0.00082, 0],
     [0, 0, 0, 0],
   ])
 
@@ -45,77 +42,85 @@ struct KalmanFilter {
   /// 1도 일 때 몇 km? = 지구 반지름 6371 * cos(37) * 1 (pi/180) = 85.18km
   /// 1º : 85180m = y : 10m
   /// y = 0.00011739 ~= 0.000117
-  private var r = MatrixOfTwoDimension([
-    [0.000899, 0],
-    [0, 0.000117],
+  private var r = simd_double2x2([
+    [0.00082, 0],
+    [0, 0.00082],
   ])
 
-  var prevHeadingValue: Double
-  var prevSpeedAtLatitude: Double = 0
-  var prevSpeedAtLongitude: Double = 0
-
   /// 관계 식 입니다.
-  lazy var A = MatrixOfTwoDimension([
-    [1, cos(prevHeadingValue) * prevSpeedAtLatitude, 0, 0],
+  lazy var A = simd_double4x4([
+    [1, timeInterval * prevVelocity.lat, 0, 0],
     [0, 1, 0, 0],
-    [0, 0, 1, sin(prevHeadingValue) * prevSpeedAtLongitude],
+    [0, 0, 1, timeInterval * prevVelocity.long],
     [0, 0, 0, 1],
   ])
 
-  /// 우리가 궁금한건 위도와 경도이기 때문에 필요한 부분만 기재했습니다.
-  private var H = MatrixOfTwoDimension([
-    [1, 0, 0, 0],
-    [0, 0, 1, 0],
+  let H = simd_double4x2([
+    [1, 0],
+    [0, 0],
+    [0, 1],
+    [0, 0],
   ])
 
-  init(initLongitude: Double, initLatitude: Double, headingValue: Double) {
-    x = .init([
-      [initLatitude],
-      [0],
-      [initLongitude],
-      [0],
-    ])
-    prevHeadingValue = headingValue
+  // 우리가 궁금한건 위도와 경도이기 때문에 필요한 부분만 기재했습니다.
+
+  var prevTime: Date
+  init(initLocation: CLLocation) {
+    x = .init(initLocation.coordinate.latitude, 0, initLocation.coordinate.longitude, 0)
+    prevTime = initLocation.timestamp
   }
 
-  /// 사용자가 가르키는 방향을 업데이트 합니다.
-  mutating func update(heading: Double) {
-    prevHeadingValue = heading
-  }
+  var timeInterval: Double = 0.0
 
-  /// Update합니다.
-  mutating func update(initLongitude: Double, initLatitude: Double, prevSpeedAtLatitude: Double, prevSpeedAtLongitude: Double) {
-    let mesure = MatrixOfTwoDimension(
+  var prevLocation = CLLocation()
+
+  let pIdentity = simd_double4x4([
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+  ])
+
+  var prevVelocity: (lat: Double, long: Double) = (0, 0)
+
+  mutating func update(currentLocation: CLLocation) {
+    let currentTime = currentLocation.timestamp
+
+    let prevTimeInterval = prevTime.timeIntervalSince1970
+    let currentTimeInterval = currentTime.timeIntervalSince1970
+
+    timeInterval = currentTimeInterval - prevTimeInterval
+
+    let velocityLatitude = (prevLocation.coordinate.latitude - currentLocation.coordinate.latitude)
+    let velocityLongitude = (prevLocation.coordinate.longitude - currentLocation.coordinate.longitude)
+
+    prevVelocity = (velocityLatitude, velocityLongitude)
+
+    prevLocation = currentLocation
+    prevTime = currentTime
+
+    let mesure = simd_double2(
       [
-        [initLatitude],
-        [initLongitude],
+        currentLocation.coordinate.latitude,
+        currentLocation.coordinate.longitude,
       ]
     )
-    self.prevSpeedAtLatitude = prevSpeedAtLatitude
-    self.prevSpeedAtLongitude = prevSpeedAtLongitude
-    guard
-      let prediction = A.multiply(x),
-      let predictionErrorCovariance = A.multiply(p)?.multiply(A.transPose())?.add(q),
 
-      let notInversed = H.multiply(predictionErrorCovariance)?.multiply(H.transPose())?.add(r),
-      let prevKalman = notInversed.invert(),
-      let kalman = predictionErrorCovariance.multiply(H.transPose())?.multiply(prevKalman),
+    let xp = A * x
+    let pp = A * p * A.transpose + q
 
-      let tempValue = H.multiply(prediction),
-      let subTractedValue = mesure.sub(tempValue),
-      let multiedKalmanValue = kalman.multiply(subTractedValue),
-      let currentX = prediction.add(multiedKalmanValue),
+    let temp = pp * H.transpose
+    let invert = (H * pp * H.transpose + r).inverse
+    let kalman = temp * invert
 
-      let tempPredictionErrorCovariance = kalman.multiply(H)?.multiply(predictionErrorCovariance),
-      let currentPredictionErrorCovariance = predictionErrorCovariance.sub(tempPredictionErrorCovariance)
-    else {
-      return
-    }
+    let currentX = xp + kalman * (mesure - H * xp)
+
+    let currentP = pp - kalman * H * pp.inverse
     x = currentX
-    p = currentPredictionErrorCovariance
+    p = currentP
   }
 
   var latestCensoredPosition: KalmanFilterCensored {
-    return .init(longitude: x.value[2][0], latitude: x.value[0][0])
+    return .init(longitude: x[2], latitude: x[0])
   }
 }
