e8cb6f6123e2927bba3530630c696512889dc98a
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-01-12 10:58:59 +0900

    [GWL-405] 무한스크롤 기능 구현 (#439)
    * feat: HomeFeature DownSampling 모듈 추가
    
    * feat: 다운 샘플링 적용
    
    * feat: ImageCacher 및 PrepareForReuse 적용
    
    * feat: Repository And UseCase 파일 생성
    
    * feat: FetchCheckManager 구현
    
    * move: 파일 위치 변경
    
    * feat: coordaintor 및 UseCase 적용
    
    * feat: 무한 스크롤 구현
    
    * delete: 목 데이터 삭제
    
    * feat: 화면에 피드가 표시되었을 때 Page넘버를 증가하는 기능 추가
    
    * delete: Home화면 Resources 폴더 제거
    
    * style: 피드백 적용 및 쓰지 않는 import문 제거

--- iOS/Projects/Features/Home/Sources/Presntaion/HomeScene/VIew/FeedImageCell.swift
+++ iOS/Projects/Features/Home/Sources/Presntaion/HomeScene/VIew/FeedImageCell.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2023 kr.codesquad.boostcamp8. All rights reserved.
 //
 
+import Cacher
 import UIKit
 
 // MARK: - FeedImageCell
@@ -22,6 +23,12 @@ final class FeedImageCell: UICollectionViewCell {
   @available(*, unavailable)
   required init?(coder _: NSCoder) {
     fatalError("cant use this init")
+  }
+
+  override func prepareForReuse() {
+    super.prepareForReuse()
+
+    feedImage.image = nil
   }
 
   private func setupViewHierarchyAndConstraints() {
@@ -43,13 +50,15 @@ final class FeedImageCell: UICollectionViewCell {
     guard let imageURL else {
       return
     }
-
-    DispatchQueue.global().async {
-      guard let data = try? Data(contentsOf: imageURL) else { return }
-      DispatchQueue.main.async { [weak self] in
-        self?.feedImage.image = UIImage(data: data)
-        self?.layoutIfNeeded()
+    guard let data = MemoryCacheManager.shared.fetch(cacheKey: imageURL.absoluteString) else {
+      DispatchQueue.global().async {
+        guard let data = try? Data(contentsOf: imageURL) else { return }
+        DispatchQueue.main.async { [weak self] in
+          self?.feedImage.image = UIImage(data: data)
+        }
       }
+      return
     }
+    feedImage.image = UIImage(data: data)
   }
 }
